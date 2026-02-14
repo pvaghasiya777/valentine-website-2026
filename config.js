c196f28bcfd3069bd6b254cf0e8a7bf083c46a88
pvaghasiya777 <46601336+pvaghasiya777@users.noreply.github.com>
2026-02-14 16:04:46 +0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dh0uhgm8u/video/upload/v1771056105/The_Best_Valentine_s_Day_Songs_with_Elmo_Sesame_Street_Songs_About_Love_and_Friendship_ehppln.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
